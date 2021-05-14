0a76f0c3df61111c5ad67c9f052bb7c65110c688
Elena Irurueta <elena@irurueta.com.ar>
2021-05-14 15:19:30 -0300

    clase vehiculo

---
+++ Irurueta-Lobos Casco-Sciutto-TP3/Irurueta-Lobos Casco-Sciutto-TP3/cVehiculo.h
@@ -0,0 +1,21 @@
+#pragma once
+template<class T>
+class cVehiculo
+{
+protected:
+	const unsigned int numeroPatente;
+	const unsigned int color;
+	const unsigned int numeroChasis;
+	const unsigned int numeroPoliza;
+	cFecha* ultimoMantenimiento;
+	float precioAlquilerXDia;
+	const unsigned int capacidadPasajeros;
+	T* elementosSeguridad;
+
+public:
+	cVehiculo();
+	virtual ~cVehiculo();
+	virtual float calcularTarifa() = 0;
+
+};
+
