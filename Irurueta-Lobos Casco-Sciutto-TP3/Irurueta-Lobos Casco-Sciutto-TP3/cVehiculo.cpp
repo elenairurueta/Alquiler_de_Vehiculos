b5573df5a60e0e1c953f19abec695f96b0bc15f0
Elena Irurueta <elena@irurueta.com.ar>
2021-05-16 15:03:33 -0300

    Clase cFecha

--- Irurueta-Lobos Casco-Sciutto-TP3/Irurueta-Lobos Casco-Sciutto-TP3/cVehiculo.cpp
+++ Irurueta-Lobos Casco-Sciutto-TP3/Irurueta-Lobos Casco-Sciutto-TP3/cVehiculo.cpp
@@ -1,1 +1,12 @@
 #include "cVehiculo.h"
+
+template<class T>
+cVehiculo<T>::cVehiculo()
+{
+}
+
+template<class T>
+cVehiculo<T>::~cVehiculo()
+{
+}
+
--- Irurueta-Lobos Casco-Sciutto-TP3/Irurueta-Lobos Casco-Sciutto-TP3/cVehiculo.h
+++ Irurueta-Lobos Casco-Sciutto-TP3/Irurueta-Lobos Casco-Sciutto-TP3/cVehiculo.h
@@ -18,4 +18,3 @@ public:
 	virtual float calcularTarifa() = 0;
 
 };
-
