77a7a7a1582e5244e673db9d672f2919ef837f27
Elena Irurueta <elena@irurueta.com.ar>
2021-05-17 10:11:58 -0300

    prueba tipos de vehículos

--- Irurueta-Lobos Casco-Sciutto-TP3/Irurueta-Lobos Casco-Sciutto-TP3/main.cpp
+++ Irurueta-Lobos Casco-Sciutto-TP3/Irurueta-Lobos Casco-Sciutto-TP3/main.cpp
@@ -12,19 +12,21 @@ int main() {
 	//UNDONE: prueba operador <<
 	cAutomovil* automovil = new cAutomovil();
 	cout << automovil << endl;
+	cAlquiler* alquiler = new cAlquiler();
+	cout << alquiler << endl;
 
 	//UNDONE: prueba operador +=
 	cListaAlquileres* lista = new cListaAlquileres();
-	cAlquiler* alquiler = new cAlquiler();
 	* lista += alquiler;
 
 	//UNDONE: prueba operador []
 	cAlquiler* otroalquiler = new cAlquiler();
 	(*lista)[0] = otroalquiler; 
 
+	//UNDONE: prueba tipos vehiculo
+	cout << automovil->getTipoVehiculo() << endl;
+	cout << automovil->calcularTarifa(2) << endl;
 	delete automovil, lista, alquiler, otroalquiler;
-
-
 
 	system("pause");
 	return 0;
