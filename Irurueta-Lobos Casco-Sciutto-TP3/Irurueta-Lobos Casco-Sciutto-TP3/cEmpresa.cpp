e7412286c477805b873c67cdbdc14a11ea3e7675
Elena Irurueta <elena@irurueta.com.ar>
2021-05-17 10:30:50 -0300

    clase cEmpresa

--- Irurueta-Lobos Casco-Sciutto-TP3/Irurueta-Lobos Casco-Sciutto-TP3/cEmpresa.cpp
+++ Irurueta-Lobos Casco-Sciutto-TP3/Irurueta-Lobos Casco-Sciutto-TP3/cEmpresa.cpp
@@ -1,1 +1,24 @@
 #include "cEmpresa.h"
+
+cEmpresa::cEmpresa(cListaT<cVehiculo>* listaVehiculos, cListaAlquileres* listaAlquileres, cListaT<cCliente>* listaClientes)
+{
+	if (listaVehiculos == NULL)
+		listaVehiculos = new cListaT<cVehiculo>();
+	this->listaVehiculos = listaVehiculos;
+	if (listaAlquileres == NULL)
+		listaAlquileres = new cListaAlquileres();
+	this->listaAlquileres = listaAlquileres;
+	if (listaClientes == NULL)
+		listaClientes = new cListaT<cCliente>();
+	this->listaClientes = listaClientes;
+}
+
+cEmpresa::~cEmpresa()
+{
+	if (listaVehiculos != NULL)
+		delete listaVehiculos;
+	if (listaAlquileres != NULL)
+		delete listaAlquileres;
+	if (listaClientes != NULL)
+		delete listaClientes;
+}
--- Irurueta-Lobos Casco-Sciutto-TP3/Irurueta-Lobos Casco-Sciutto-TP3/cEmpresa.h
+++ Irurueta-Lobos Casco-Sciutto-TP3/Irurueta-Lobos Casco-Sciutto-TP3/cEmpresa.h
@@ -1,8 +1,18 @@
 #pragma once
+#include "cListaT.h"
+#include "cVehiculo.h"
+#include "cListaAlquileres.h"
+#include "cCliente.h"
+
 class cEmpresa
 {
+	cListaT<cVehiculo>* listaVehiculos;
+	cListaAlquileres* listaAlquileres;
+	cListaT<cCliente>* listaClientes;
 
-
+public:
+	cEmpresa(cListaT<cVehiculo>* listaVehiculos = NULL, cListaAlquileres* listaAlquileres = NULL, cListaT<cCliente>* listaClientes = NULL);
+	~cEmpresa();
 
 };
 
