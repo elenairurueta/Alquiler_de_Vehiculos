789586e9bf3291fbff43e7a9214c2e71393a3d9b
Elena Irurueta <84137564+elenairurueta@users.noreply.github.com>
2021-05-21 18:44:35 -0300

    pragma regions de cElementosSeguridad

--- Irurueta-Lobos Casco-Sciutto-TP3/Irurueta-Lobos Casco-Sciutto-TP3/cElementosSeguridad.h
+++ Irurueta-Lobos Casco-Sciutto-TP3/Irurueta-Lobos Casco-Sciutto-TP3/cElementosSeguridad.h
@@ -5,18 +5,31 @@ typedef enum {CASCO, SILLA_NINOS, PORTAEQUIPAJE, ASIENTOS_REVERSIBLES};
 class cElementosSeguridad
 {
 	friend class cListaElementosSeguridad;
+	
 	int tipo; //enum
 	bool agregado;
 	int cantidad;
 	const float precio;
 
 public:
+	#pragma region Constructores y Destructores
 	cElementosSeguridad(int tipo, bool agregado = false, int cantidad = 0, float precio = 0);
 	~cElementosSeguridad();
+	#pragma endregion
+	
+	#pragma region Getters
 	string getclave();
-	void setCantidad(int cantidad);
-	void setAgregado(bool agregado);
-	string toString(string separador = "\n");
 	float getPrecioTotal();
 	int getTipo();
+	#pragma endregion
+	
+	#pragma region Setters
+	void setCantidad(int cantidad);
+	void setAgregado(bool agregado);
+	#pragma endregion
+	
+	#pragma region toString() e imprimir()
+	string toString(string separador = "\n");
+	void imprimir(string separador = "\n");
+	#pragma endregion
 };
