bc4dc8047f09ab2a637b076e827932b3921e03bc
Bian Sciutto <biansc.bs@gmail.com>
2021-05-23 19:02:22 -0300

    comentarios

--- Irurueta-Lobos Casco-Sciutto-TP3/Irurueta-Lobos Casco-Sciutto-TP3/cEmpresa.cpp
+++ Irurueta-Lobos Casco-Sciutto-TP3/Irurueta-Lobos Casco-Sciutto-TP3/cEmpresa.cpp
@@ -5,13 +5,13 @@ cEmpresa::cEmpresa(float ganancia, cListaT<cVehiculo>* listaVehiculos, cListaAlq
 {
 	this->ganancia = ganancia;
 	if (listaVehiculos == NULL)
-		listaVehiculos = new cListaT<cVehiculo>();
+		listaVehiculos = new cListaT<cVehiculo>();//creamos una lista de vehiculos utilizando la template
 	this->listaVehiculos = listaVehiculos;
 	if (listaAlquileres == NULL)
-		listaAlquileres = new cListaAlquileres();
+		listaAlquileres = new cListaAlquileres();//creamos una lista de alquileres utilizando la template
 	this->listaAlquileres = listaAlquileres;
 	if (listaClientes == NULL)
-		listaClientes = new cListaT<cCliente>();
+		listaClientes = new cListaT<cCliente>();//creamos una lista de clientes utilizando la template
 	this->listaClientes = listaClientes;
 }
 cEmpresa::~cEmpresa()
@@ -27,10 +27,10 @@ cEmpresa::~cEmpresa()
 
 void cEmpresa::pasosMantenimiento(cVehiculo* vehiculo)
 {
-	//hacemos dynamic cast para idenificar que vehículo es el pasado por parámetro
+	//hacemos dynamic cast para identificar que vehículo es el pasado por parámetro e imprimimos los pasos mantenimiento que correspondan
 	cAutomovil* ptrAutomovil = dynamic_cast<cAutomovil*>(vehiculo);
 	if (ptrAutomovil != NULL)
-		cout << "\n\nPasos Mantenimiento AUTOMOVIL" << cAutomovil::getPasosMantenimiento() << endl; //e imprimimos los pasos mantenimiento que correspondan
+		cout << "\n\nPasos Mantenimiento AUTOMOVIL" << cAutomovil::getPasosMantenimiento() << endl; 
 	cCamioneta* ptrCamioneta = dynamic_cast<cCamioneta*>(vehiculo);
 	if (ptrCamioneta != NULL)
 		cout << "\n\nPasos Mantenimiento CAMIONETA" << cCamioneta::getPasosMantenimiento() << endl;
@@ -45,45 +45,47 @@ void cEmpresa::mantenimiento(cVehiculo* vehiculo)
 {
 	if(vehiculo == NULL)
 		return;
-	if(listaAlquileres->chequearVehiculoEnAlquiler(vehiculo) == true){
+	if(listaAlquileres->chequearVehiculoEnAlquiler(vehiculo) == true){ //nos fijamos que el vehículo no este alquilado
 		cout << "\n\nNo se puede realizar el mantenimiento del vehiculo " << vehiculo->getclave() << " porque se encuentra alquilado." << endl;
 		return;
 	}
 	cout << "\n\nVehiculo en mantenimiento: " << vehiculo << endl;
-	pasosMantenimiento(vehiculo);
-	vehiculo->actualizarUltimoMantenimiento();
+	pasosMantenimiento(vehiculo);//imprimimos los pasos de su mantenimiento
+	vehiculo->actualizarUltimoMantenimiento();//actualizamos la fecha de su último mantenimiento
 }
-void cEmpresa::adquirirVehiculo(cVehiculo* vehiculo)
+void cEmpresa::adquirirVehiculo(cVehiculo* vehiculo)//pasamos por parámetro el vehículo a adquirir por la empresa
 {
 	if(vehiculo == NULL)
 		return;
-	listaVehiculos->AgregarItem(vehiculo);
-	ganancia -= vehiculo->getPrecioCompraVehiculo();
+	listaVehiculos->AgregarItem(vehiculo);//agregamos el vehículo a la lista de vehículos
+	ganancia -= vehiculo->getPrecioCompraVehiculo();//le restamos a la ganancia el precio del vehículo
 }
 cVehiculo* cEmpresa::sacarCirculacionVehiculo(string clave)
 {
+	//buscamos por patente al vehículo en la lista y lo igualamos a un puntero de cVehiculo
 	cVehiculo* vehiculo = listaVehiculos->BuscarItem(clave);
-	if (vehiculo == NULL)
+	if (vehiculo == NULL)//si no lo encuentra devuelve NULL
 		return NULL;
-	if (listaAlquileres->chequearVehiculoEnAlquiler(vehiculo))
+	if (listaAlquileres->chequearVehiculoEnAlquiler(vehiculo))//si lo encuentra y está en alquiler también devuelve NULL
 		return NULL;
-	cVehiculo* vehiculoQuitado = listaVehiculos->Quitar(vehiculo);
+	cVehiculo* vehiculoQuitado = listaVehiculos->Quitar(vehiculo);//saca al vehículo de la lista
 	return vehiculoQuitado;
 }
 void cEmpresa::nuevoAlquiler(cAlquiler* alquiler)
 {
 	if(alquiler == NULL)
 		return;
+	//buscamos por DNI al cliente en la lista y lo igualamos a un puntero de cCliente
 	cCliente* clienteBuscar = listaClientes->BuscarItem(alquiler->getCliente()->getclave());
-	if (clienteBuscar == NULL) {
+	if (clienteBuscar == NULL) {//si no lo encuentra, agrega al cliente en la lista de clientes
 		(*listaClientes) += (alquiler->getCliente());
 	}
-	ganancia += alquiler->actualizarMontoTotal();
-	bool sePudoAgregar = listaAlquileres->AgregarItem(alquiler);
-	if (!sePudoAgregar)
+	ganancia += alquiler->actualizarMontoTotal();//sumamos el monto del nuevo alquiler a la ganancia
+	bool sePudoAgregar = listaAlquileres->AgregarItem(alquiler);//lo agregamos a la lista de alquileres
+	if (!sePudoAgregar)//si no se pudo agregar arroja una excepción
 		throw new exception("El alquiler que se está intentado agregar ya se encuentra en la lista");
 }
-cListaAlquileres* cEmpresa::terminarAlquileres(cFecha* fechaActual)
+cListaAlquileres* cEmpresa::terminarAlquileres(cFecha* fechaActual)//si concluyó su alquiler, lo quitamos de la lista
 {
 	if(fechaActual == NULL)
 		return NULL;
@@ -93,6 +95,7 @@ cListaAlquileres* cEmpresa::terminarAlquileres(cFecha* fechaActual)
 #pragma region Getters
 cVehiculo* cEmpresa::getVehiculoCategoria(string categoria)
 {
+	//utilizamos dynamic_cast para fijarnos a que clase hija pertenece, devuelve NULL si no corresponde
 	for (int i = 0; i < listaVehiculos->getCA(); i++) {
 
 		if (categoria == "automovil") {
