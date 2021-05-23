0250be11b2f14caae054daf2b9bfad5bbd079f63
VLobosCasco <valenlobosc@gmail.com>
2021-05-23 19:09:26 -0300

    Comentarios

--- Irurueta-Lobos Casco-Sciutto-TP3/Irurueta-Lobos Casco-Sciutto-TP3/funciones.cpp
+++ Irurueta-Lobos Casco-Sciutto-TP3/Irurueta-Lobos Casco-Sciutto-TP3/funciones.cpp
@@ -9,8 +9,9 @@ void agregarVehiculos(int cantidad, string tipo, cEmpresa* miEmpresa)
 	cVehiculo* miVehiculo = NULL;
 	srand(time(NULL));
 	cFecha fechaMantenimiento;
+	//Recorro la cantidad de los vehiculos a agregar y verifico que categoria de vehiculo es
 	for (int i = 0; i < cantidad; i++) {
-		fechaMantenimiento.actualizarFecha();
+		fechaMantenimiento.actualizarFecha(); //La ultima fecha de mantenimiento se determina como la fecha de adquisicion del mismo
 		if (tipo.compare("automovil") == 0)
 			miVehiculo = new cAutomovil(PATENTES[cantVehiculosAgregados], (rand() % 5), NUMEROSDECHASIS[cantVehiculosAgregados], NUMEROPOLIZA[cantVehiculosAgregados]);
 		else if (tipo.compare("camioneta") == 0)
@@ -19,9 +20,9 @@ void agregarVehiculos(int cantidad, string tipo, cEmpresa* miEmpresa)
 			miVehiculo = new cTrafic(PATENTES[cantVehiculosAgregados], (rand() % 5), NUMEROSDECHASIS[cantVehiculosAgregados], NUMEROPOLIZA[cantVehiculosAgregados]);
 		else if (tipo.compare("motocicleta") == 0)
 			miVehiculo = new cMotocicleta(PATENTES[cantVehiculosAgregados], (rand() % 5), NUMEROSDECHASIS[cantVehiculosAgregados], NUMEROPOLIZA[cantVehiculosAgregados]);
-		cantVehiculosAgregados++;
+		cantVehiculosAgregados++; //Aumento la variable global de vehiculos agregados
 		try {
-			miEmpresa->adquirirVehiculo(miVehiculo);
+			miEmpresa->adquirirVehiculo(miVehiculo); // Se agrega a la empresa el vehiculo 
 		}
 		catch (exception* ex) {
 			cout << ex->what() << endl; delete ex;
@@ -29,7 +30,9 @@ void agregarVehiculos(int cantidad, string tipo, cEmpresa* miEmpresa)
 	}
 }
 void retirarVehiculoRandom(cEmpresa* miEmpresa) {
+	//Le paso una patente de la lista de patentes creada en "datosVehiculo.h" para sacar de circulacion
 	cVehiculo* vehiculoQuitado = miEmpresa->sacarCirculacionVehiculo(PATENTES[rand() % cantVehiculosAgregados]);
+	//Si no la encuentra o esta en alquiler, devuelve NULL
 	if(vehiculoQuitado == NULL)
 		cout << "El vehiculo no puede ser sacado de circulacion porque se encuentra alquilado" << endl;
 	else 
@@ -39,21 +42,24 @@ void retirarVehiculoRandom(cEmpresa* miEmpresa) {
 void nuevosAlquileres(int cantidad, cEmpresa* miEmpresa) {
 	cAlquiler* miAlquiler = NULL; cCliente* miCliente = NULL; cVehiculo* miVehiculo = NULL;
 	srand(time(NULL));
+	//Recorro la cantidad de alquileres a agregar 
 	for (int i = 0; i < cantidad; i++)
-	{
+	{   //Creo un cliente de la los datos de clientes en "datosClientes.h"
 		miCliente = new cCliente(NOMBRES[cantClientesAgregados], DNIS[cantClientesAgregados], TELEFONOS[cantClientesAgregados]);
-		
+		//Busco un vehiculo de la categoria deseada
 		miVehiculo = miEmpresa->getVehiculoCategoria(CATEGORIAS[rand() % cantCATEGORIAS]);
+		//Si no encuentra un vehiculo disponible de la categoria deseada devuekve NULL
 		if (miVehiculo == NULL) {
 			cout << "No hay vehiculos disponibles de la categoria solicitada" << endl;
 			continue;
 		}
 		cFecha* fechaInicio = new cFecha(), *fechaFin = new cFecha();
+		//Determino la fecha de inicio, fin y duracion del alquiler
 		getFechasRandomNuevoAlquiler(fechaInicio, fechaFin);
 		miAlquiler = new cAlquiler(fechaInicio, fechaFin, miCliente, miVehiculo);
 		agregarElementosSeguridad(miAlquiler, miVehiculo);
 		try {
-			miEmpresa->nuevoAlquiler(miAlquiler);
+			miEmpresa->nuevoAlquiler(miAlquiler); //Agregamos el alquiler a la lista de alquileres de la empresa
 		}
 		catch (exception* ex) {
 			cout << ex->what() << endl; delete ex;
@@ -63,6 +69,7 @@ void nuevosAlquileres(int cantidad, cEmpresa* miEmpresa) {
 
 }
 void getFechasRandomNuevoAlquiler(cFecha* fechaInicio, cFecha* fechaFin) {
+	//Definimos dos fechas posteriores a la actual para la de inicio y fin del alquiler
 	if ((fechaInicio == NULL) || (fechaFin == NULL))
 		return;
 	fechaInicio->actualizarFecha(); fechaFin->actualizarFecha();
@@ -72,6 +79,7 @@ void getFechasRandomNuevoAlquiler(cFecha* fechaInicio, cFecha* fechaFin) {
 	fechaFin->incrementarFecha(randomDiasInicio + randomDiasDiferencia, randomMesesInicio + randomMesesDiferencia);
 }
 void agregarElementosSeguridad(cAlquiler* miAlquiler, cVehiculo* miVehiculo) {
+	//Recorremos y agregamos una cantidad random de los elementos de seguridad disponible 
 	for (int i = 0; i < miVehiculo->getCantidadElementosSeguridad(); i++) {
 		int elementoSeguridad = miAlquiler->getElementoSeguridad(i);
 		int cantidadESagregar = rand() % 3; //si es más o menos de lo permitido, se arregla en agregarElementoSeguridad()
@@ -79,6 +87,7 @@ void agregarElementosSeguridad(cAlquiler* miAlquiler, cVehiculo* miVehiculo) {
 	}
 }
 void mantenimientoVehiculoRandom(cEmpresa* miEmpresa) {
+	//Llamamos al método mantenimiento de empresa para imprimir los pasos de mantenimiento de un vehiculo random 
 	miEmpresa->mantenimiento(miEmpresa->getVehiculo(PATENTES[rand() % cantVehiculosAgregados]));
 }
 void imprimirFechaActual() {
@@ -86,11 +95,11 @@ void imprimirFechaActual() {
 	cout << "\n\n\t\t\t[ " << fechaActual << " ]\n\n";
 }
 void diaSiguiente(cEmpresa* miEmpresa)
-{
+{// Incrementamos de a 1 dia la fecha actual 
 	fechaActual->incrementarFecha(1);
 	cListaAlquileres* alquileresQuitados = NULL;
 	try {
-		alquileresQuitados = miEmpresa->terminarAlquileres(fechaActual);
+		alquileresQuitados = miEmpresa->terminarAlquileres(fechaActual); //Terminamos los alquileres que tengan como fecha de fin del alquiler igual a la fecha actual
 	}
 	catch (exception* ex) {
 		cout << ex->what() << endl;
@@ -105,7 +114,7 @@ void diaSiguiente(cEmpresa* miEmpresa)
 		delete alquileresQuitados;
 	}
 	else {
-		cout << "\n\nAlquileres finalizados hoy: \n" << alquileresQuitados->toString("\n\t") << endl;
+		cout << "\n\nAlquileres finalizados hoy: \n" << alquileresQuitados->toString("\n\t") << endl; //Imprimimos los alquileres finalizados en la fecha 
 		delete alquileresQuitados;
 	}
 	system("pause");
--- Irurueta-Lobos Casco-Sciutto-TP3/Irurueta-Lobos Casco-Sciutto-TP3/funciones.h
+++ Irurueta-Lobos Casco-Sciutto-TP3/Irurueta-Lobos Casco-Sciutto-TP3/funciones.h
@@ -3,16 +3,22 @@
 #include "datosVehiculos.h"
 #include "datosClientes.h"
 
-
+// variables goblales  
 extern cFecha *fechaActual;
 extern int cantVehiculosAgregados;
 extern int cantClientesAgregados;
 
+//Agregar 1 vehiculos de un tipo rad
 void agregarVehiculos(int cantidad = 1, string tipo = "", cEmpresa* miEmpresa = NULL);
+//Retiramos de la empresa un vehiculo random 
 void retirarVehiculoRandom(cEmpresa* miEmpresa);
+//Crea un nuevo alquiler junto con sus atributos 
 void nuevosAlquileres(int cantidad, cEmpresa* miEmpresa);
 void getFechasRandomNuevoAlquiler(cFecha* fechaInicio, cFecha* fechaFin);
+//Agrega un numero random (dentro de lo permitido) de c/elemento de seguridad de la categoria
 void agregarElementosSeguridad(cAlquiler* miAlquiler, cVehiculo* miVehiculo);
+//Imprime los pasos de mantenimiento de un vehiculo random 
 void mantenimientoVehiculoRandom(cEmpresa* miEmpresa);
 void imprimirFechaActual();
+//Pasa al siguiente dia y verifica cual es el alquiler que finaliza ese dia 
 void diaSiguiente(cEmpresa* miEmpresa);
